c7a7e5f508b9685cfdbfdc7a222023a8c337b57d
andreqko <andreqkodev@gmail.com>
2024-02-11 00:09:02 +0200

    Feat: add custom icons

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,6 @@
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import iconMoonConfig from '../../selection.json';
+
+const Icon = createIconSetFromIcoMoon(iconMoonConfig);
+
+export default Icon;
--- src/screens/Auth/Welcome/WelcomeScreen.tsx
+++ src/screens/Auth/Welcome/WelcomeScreen.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useStyles } from 'react-native-unistyles';
-import fontStyleSheet from 'styles/fonts.ts';
+
+import fontStyleSheet from '@styles/fonts.ts';
+import Icon from '@components/Icon/Icon';
 
 const WelcomeScreen = () => {
   const { styles } = useStyles(fontStyleSheet);
@@ -10,6 +12,7 @@ const WelcomeScreen = () => {
   return (
     <SafeAreaView>
       <Text style={styles.textXxsmRegular}>Welcome</Text>
+      <Icon name="power" />
       <Text style={styles.textXxsmExtrabold}>Welcome Boldie</Text>
     </SafeAreaView>
   );
